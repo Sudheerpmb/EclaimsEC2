a45d6fb7a55ac956de0c08b76955d1746e91a706
root <naveen.ch@softforceapps.com>
2023-10-06 12:26:32 +0000

    new

--- js/lib/const.js
+++ js/lib/const.js
@@ -1,11 +1,10 @@
-
 // Enable for Local Development
 // var env = {
 //     "api_url":"https://wrapperapi.europassistance.in",
-//     // "api_url":"https://ecmware.europ-assistance.in/",  
-//     "app_url":"http://localhost/html_d/",    
-//     // "node_api_url":"http://localhost:3000" ,  
-// "node_api_url":"https://eztraveluat.europassistance.in:3000/"  ,
+//     // "api_url":"https://ecmware.europ-assistance.in/",
+//     "app_url":"http://localhost/live_uat/",
+//     // "node_api_url":"http://localhost:3000" ,
+// "node_api_url":"https://eztraveluat.europassistance.in:3000/" ,
 // "token":'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwODdhYzg1ZGYwMDY2MjQ1MDBiYzdiNCIsInJvbGUiOiJjdXN0b21lciIsImV4cCI6MTYyNjUxMTY3OCwiaWF0IjoxNjIxMzI3Njc4fQ.IKeo_5iW4mwa7jUkRW0WxfZK3Oqd-Spqn6UoIWDfMeE'
 // }
 
@@ -13,29 +12,27 @@
 
 
 // Enable for Prod
-var env = {
-    "app_url":"https://eclaims.europ-assistance.in/",
-    "node_api_url":"https://eztravelapi2.europassistance.in/"
-}
-
-
-// Enable for uat
 // var env = {
-//     "api_url":"https://wrapperapi.europassistance.in",    
-//     "app_url":"https://eclaimsuat.europassistance.in/",
-//     "node_api_url":"https://eztraveluat.europassistance.in:3000/"    
+//     "api_url":"https://ecmware.europ-assistance.in",
+//     "app_url":"https://eclaims.europ-assistance.in/",
+//     "node_api_url":"http://203.175.177.45:3000/"
 // }
 
+// Enable for uat
+var env = {
+    "app_url":"https://eclaimsuat.europassistance.in/",
+    "node_api_url":"https://eztraveluatapi.europassistance.in/"
+}
 
 
 // Enable for Prod
 // var env = {
-//     "api_url":"https://ecmware.europ-assistance.in/",    
+//     "api_url":"https://ecmware.europ-assistance.in/",
 //     "app_url":"https://eclaims.europ-assistance.in/",
 //     // "app_url":"http://eclaimsuat.europassistance.in/",
-//     "node_api_url":"https://eztraveluat.europassistance.in:3000/",
+//     "node_api_url":env.node_api_url+"",
 //     "saxClients": ["Reliance"],
-//     "ezTravel":["MCHI","TATAAIG"]    
+//     "ezTravel":["MCHI","TATAAIG"]
 // }
 
 var hash={
