462c4a25a763a632ff6e908cdb903c3c94519522
vmishra576 <124881774+vmishra576@users.noreply.github.com>
2023-11-24 10:06:24 +0530

    url change

--- NearestHospitals/controllers/main.js
+++ NearestHospitals/controllers/main.js
@@ -41,7 +41,7 @@ function initMap() {
     $.ajax({
         async: true,
         crossDomain: true,
-        url: `https://eztraveluatapi.europassistance.in/api/nearestlocations?lattitude=${cordi.lng}&longitude=${cordi.lat}`,
+        url: `${env.node_api_url}api/nearestlocations?lattitude=${cordi.lng}&longitude=${cordi.lat}`,
         type: "GET",
         contentType: false,
         processData: false,
@@ -319,9 +319,9 @@ function initMap() {
                     type = ''
                 let urlgeo = ''
                 if (pin === "")
-                    urlgeo = `https://eztraveluatapi.europassistance.in/eclaims/locationssearch?city=${city}&type=${type}&limit=${limit}`
+                    urlgeo = `${env.node_api_url}eclaims/locationssearch?city=${city}&type=${type}&limit=${limit}`
                 else
-                    urlgeo = `https://eztraveluatapi.europassistance.in/eclaims/locationssearch?zip=${pin}&type=${type}&limit=${limit}`
+                    urlgeo = `${env.node_api_url}eclaims/locationssearch?zip=${pin}&type=${type}&limit=${limit}`
                 $.ajax({
                     async: true,
                     crossDomain: true,
